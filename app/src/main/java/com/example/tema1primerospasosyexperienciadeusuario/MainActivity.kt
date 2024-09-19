12cabbf77de039de1137f81cf217edfb1531169b
marcos <marcgabe@myuax.com>
2024-09-19 12:56:24 +0200

    Lanzamiento de app
    Pasando por aqui (que se me habia olvidado) la actividad de lanzamento de la app definitiva

--- app/src/main/java/com/example/tema1primerospasosyexperienciadeusuario/MainActivity.kt
+++ app/src/main/java/com/example/tema1primerospasosyexperienciadeusuario/MainActivity.kt
@@ -3,45 +3,35 @@ package com.example.tema1primerospasosyexperienciadeusuario
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tema1primerospasosyexperienciadeusuario.ui.theme.Tema1PrimerosPasosyExperienciadeUsuarioTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.tema1primerospasosyexperienciadeusuario.ui.theme.PantallaInicio
+import com.example.tema1primerospasosyexperienciadeusuario.ui.theme.PantallaPrincipal
+import com.example.tema1primerospasosyexperienciadeusuario.ui.theme.PantallaConfiguracion
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Tema1PrimerosPasosyExperienciadeUsuarioTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MyApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyApp() {
+    val navController = rememberNavController()
+    var backgroundColor by remember { mutableStateOf(Color.Gray) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Tema1PrimerosPasosyExperienciadeUsuarioTheme {
-        Greeting("Android")
+    NavHost(navController = navController, startDestination = "pantalla_inicio") {
+        composable("pantalla_inicio") { PantallaInicio(navController, backgroundColor) }
+        composable("pantalla_principal") { PantallaPrincipal(navController, backgroundColor) }
+        composable("pantalla_configuracion") { PantallaConfiguracion(navController, backgroundColor) { newColor ->
+            backgroundColor = newColor
+        }}
     }
 }
