b6587abb333aa805a4f0abd411e2e5aff4e036a3
marcos <marcgabe@myuax.com>
2024-09-21 14:28:09 +0200

    test de pantalla principal/intermedia
    Aqui si que hay un poco mas de contenido (couidado con el sdk marcos)

---
+++ app/src/androidTest/java/com/example/tema1primerospasosyexperienciadeusuario/PantallaPrincipalTest.kt
@@ -0,0 +1,27 @@
+// PantallaPrincipalTest.kt
+package com.example.tema1primerospasosyexperienciadeusuario.ui.theme
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.navigation.compose.rememberNavController
+import org.junit.Rule
+import org.junit.Test
+
+class PantallaPrincipalTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testPantallaPrincipal() {
+        composeTestRule.setContent {
+            PantallaPrincipal(navController = rememberNavController(), backgroundColor = Color.Gray)
+        }
+
+        composeTestRule.onNodeWithText("Aquí nombre").performTextInput("Test Name")
+        composeTestRule.onNodeWithText("Guarda Nombre").performClick()
+        composeTestRule.onNodeWithText("Guardar Nombre: Test Name").assertExists()
+    }
+}
