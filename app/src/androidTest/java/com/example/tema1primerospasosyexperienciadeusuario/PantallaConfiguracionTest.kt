af3128103a4f5e0bde51b49a2a9c02de10c48884
marcos <marcgabe@myuax.com>
2024-09-21 14:27:21 +0200

    Test para el tema de la config
    muy por encima

---
+++ app/src/androidTest/java/com/example/tema1primerospasosyexperienciadeusuario/PantallaConfiguracionTest.kt
@@ -0,0 +1,25 @@
+// PantallaConfiguracionTest.kt
+package com.example.tema1primerospasosyexperienciadeusuario.ui.theme
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.rememberNavController
+import org.junit.Rule
+import org.junit.Test
+
+class PantallaConfiguracionTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testPantallaConfiguracion() {
+        composeTestRule.setContent {
+            PantallaConfiguracion(navController = rememberNavController(), backgroundColor = Color.Gray, onColorChange = {})
+        }
+
+        composeTestRule.onNodeWithText("Rojo").performClick()
+        composeTestRule.onNodeWithText("Volver a la Pantalla de Inicio").performClick()
+    }
+}
