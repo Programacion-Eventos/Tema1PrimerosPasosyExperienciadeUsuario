bc458cbb3820aa620fcfc576b9f31e4234663928
marcos <marcgabe@myuax.com>
2024-09-21 14:27:39 +0200

    Test para el tema del inicio
    Poca chicha

---
+++ app/src/androidTest/java/com/example/tema1primerospasosyexperienciadeusuario/PantallaInicioTest.kt
@@ -0,0 +1,24 @@
+// PantallaInicioTest.kt
+package com.example.tema1primerospasosyexperienciadeusuario.ui.theme
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.rememberNavController
+import org.junit.Rule
+import org.junit.Test
+
+class PantallaInicioTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testPantallaInicio() {
+        composeTestRule.setContent {
+            PantallaInicio(navController = rememberNavController(), backgroundColor = Color.Gray)
+        }
+
+        composeTestRule.onNodeWithText("IR A PRINCIPAL").assertExists().performClick()
+    }
+}
