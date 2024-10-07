b30a32509e5ec64be792686b486716bd9f0d779f
marcos <marcgabe@myuax.com>
2024-10-07 20:19:02 +0200

    Update SegundoPlano.kt
    chorrada de espacio

--- app/src/main/java/com/example/tema1primerospasosyexperienciadeusuario/ui/theme/SegundoPlano.kt
+++ app/src/main/java/com/example/tema1primerospasosyexperienciadeusuario/ui/theme/SegundoPlano.kt
@@ -2,11 +2,10 @@ package com.example.tema1primerospasosyexperienciadeusuario.ui.theme
 
 import android.os.AsyncTask
 
-
 class SegundoPlano(
     private val actualizarProgreso: (Int) -> Unit,
     private val tareaFinalizada: () -> Unit
-) : AsyncTask<Void, Int, Void>()  {
+) : AsyncTask<Void, Int, Void>() {
 
     override fun onPreExecute() {
         super.onPreExecute()
