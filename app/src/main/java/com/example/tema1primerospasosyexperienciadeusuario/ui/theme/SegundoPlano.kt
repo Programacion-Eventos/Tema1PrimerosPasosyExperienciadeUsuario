059c7db9251b0808d3adf7f1569e4153f1b0b96b
marcos <marcgabe@myuax.com>
2024-10-07 20:16:18 +0200

    Update SegundoPlano.kt
    Este método se ejecuta en el hilo de la interfaz de usuario antes de que comience la tarea en segundo plano. Se puede usar para inicializar elementos de la UI, como mostrar una barra de progreso.

--- app/src/main/java/com/example/tema1primerospasosyexperienciadeusuario/ui/theme/SegundoPlano.kt
+++ app/src/main/java/com/example/tema1primerospasosyexperienciadeusuario/ui/theme/SegundoPlano.kt
@@ -6,4 +6,10 @@ import android.os.AsyncTask
 class SegundoPlano(
     private val actualizarProgreso: (Int) -> Unit,
     private val tareaFinalizada: () -> Unit
-) : AsyncTask<Void, Int, Void>()  {}
+) : AsyncTask<Void, Int, Void>()  {
+
+    override fun onPreExecute() {
+        super.onPreExecute()
+        // Inicializar elementos de la UI si es necesario
+    }
+}
