7b8bcc57b5e74dd11b2f220d1605268599c683e3
marcos <marcgabe@myuax.com>
2024-11-04 23:44:10 +0100

    Create Firebase.java
    Para el uso de la real time database de firebase

---
+++ app/src/main/java/com/example/tema1primerospasosyexperienciadeusuario/model/Firebase.java
@@ -0,0 +1,19 @@
+package com.example.tema1primerospasosyexperienciadeusuario.model;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Firebase {
+    private final DatabaseReference database;
+
+    public Firebase() {
+        database = FirebaseDatabase.getInstance("https://taller-e86f1-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
+    }
+
+    public void saveNombre(String nombre) {
+        String key = database.child("nombres").push().getKey();
+        if (key != null) {
+            database.child("nombres").child(key).setValue(nombre);
+        }
+    }
+}
