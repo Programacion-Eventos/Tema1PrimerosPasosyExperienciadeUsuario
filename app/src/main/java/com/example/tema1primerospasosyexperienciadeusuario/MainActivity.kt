85f7f522f2c486692fe5fe8c6d672ac0583b3dc5
marcos <marcgabe@myuax.com>
2024-11-04 21:15:17 +0100

    Update MainActivity.kt
    Evolucion global actual del main en base a las nuevas adiciones y migracion

--- app/src/main/java/com/example/tema1primerospasosyexperienciadeusuario/MainActivity.kt
+++ app/src/main/java/com/example/tema1primerospasosyexperienciadeusuario/MainActivity.kt
@@ -3,35 +3,45 @@ package com.example.tema1primerospasosyexperienciadeusuario
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.tema1primerospasosyexperienciadeusuario.ui.theme.PantallaInicio
-import com.example.tema1primerospasosyexperienciadeusuario.ui.theme.PantallaPrincipal
-import com.example.tema1primerospasosyexperienciadeusuario.ui.theme.PantallaConfiguracion
-import androidx.compose.ui.graphics.Color
+import com.example.tema1primerospasosyexperienciadeusuario.model.PreferenceManager
+import com.example.tema1primerospasosyexperienciadeusuario.vista.PantallaConfiguracion
+import com.example.tema1primerospasosyexperienciadeusuario.vista.PantallaInicio
+import com.example.tema1primerospasosyexperienciadeusuario.vista.PantallaPrincipal
 
 class MainActivity : ComponentActivity() {
+    private lateinit var preferencesManager: PreferenceManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        preferencesManager = PreferenceManager(this)
         setContent {
-            MyApp()
+            MyApp(preferencesManager)
         }
     }
 }
 
 @Composable
-fun MyApp() {
+fun MyApp(preferencesManager: PreferenceManager) {
     val navController = rememberNavController()
-    var backgroundColor by remember { mutableStateOf(Color.Gray) }
+    var userName by remember { mutableStateOf(preferencesManager.getUserName()) }
+    var backgroundColor by remember { mutableStateOf(Color(preferencesManager.getBackgroundColor(userName))) }
 
     NavHost(navController = navController, startDestination = "pantalla_inicio") {
         composable("pantalla_inicio") { PantallaInicio(navController, backgroundColor) }
-        composable("pantalla_principal") { PantallaPrincipal(navController, backgroundColor) }
+        composable("pantalla_principal") { PantallaPrincipal(navController, backgroundColor, preferencesManager) { newName, newColor ->
+            userName = newName
+            backgroundColor = newColor
+            preferencesManager.saveUserNameAndColor(newName, newColor.toArgb())
+        }}
         composable("pantalla_configuracion") { PantallaConfiguracion(navController, backgroundColor) { newColor ->
             backgroundColor = newColor
+            preferencesManager.saveUserNameAndColor(userName, newColor.toArgb())
         }}
     }
 }
