a3bcba66c0e798fc7c83114253dddc89c4978686
marcos <marcgabe@myuax.com>
2024-11-04 21:14:22 +0100

    CONFIG READAPTACION BRUTAL
    Le damos una pasada tremenda a todos los aspectos extrapolados de configuracion veriones y dependencias:
    
    (Las reutilizamos de codigo previo de otros trabajos de eventos)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt) // Asegúrate de que esta línea esté incluida
+    id("com.google.gms.google-services") version libs.versions.googleServices.get() // Aplica el plugin de Google Services
 }
 
 android {
@@ -9,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.tema1primerospasosyexperienciadeusuario"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -47,10 +49,18 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    sourceSets {
+        getByName("main") {
+            manifest.srcFile("src/main/AndroidManifest.xml")
+            java.srcDirs("src/main/java")
+            res.srcDirs("src/main/res")
+        }
+    }
+
+    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -59,8 +69,14 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+    //implementation(libs.androidx.room.runtime) // Dependencia de Room runtime
+    //kapt(libs.androidx.room.compiler) // Dependencia de Room compiler
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.runtime.livedata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -68,4 +84,26 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Firebase dependencies
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.messaging)
+    implementation(libs.androidx.multidex)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+
+    //extras
+    implementation (libs.androidx.work.runtime.ktx)
 }
+
+// Aplica el plugin de Google Services
+apply(plugin = "com.google.gms.google-services")
+
+/*testImplementation(libs.junit)
+testImplementation(libs.kotlinx.coroutines.test.v164)
+androidTestImplementation(libs.ui.test.junit4)
+androidTestImplementation(libs.androidx.junit.v113)
+androidTestImplementation(libs.androidx.espresso.core.v340)
+testImplementation(libs.kotlinx.coroutines.test)
+*/
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Archivo de nivel superior donde puedes agregar opciones de configuración comunes a todos los subproyectos/módulos.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false // Asegúrate de que esta línea esté incluidaz
+    id("com.google.gms.google-services") version "4.4.2" apply false // Aplica el plugin de Google Services
 }
