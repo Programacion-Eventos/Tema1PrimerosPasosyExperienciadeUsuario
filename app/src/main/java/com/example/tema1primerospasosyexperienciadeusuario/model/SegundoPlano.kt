4664ccdd645474dc794a3a52042b08f8ff44a96e
marcos <marcgabe@myuax.com>
2024-11-04 21:11:32 +0100

    Create SegundoPlano.kt
    Pasamos la clase de logica en sugundo plano para la barra a un nuevo paquete modelo

---
+++ app/src/main/java/com/example/tema1primerospasosyexperienciadeusuario/model/SegundoPlano.kt
@@ -0,0 +1,32 @@
+package com.example.tema1primerospasosyexperienciadeusuario.model
+
+import android.os.AsyncTask
+
+class SegundoPlano(
+    private val actualizarProgreso: (Int) -> Unit,
+    private val tareaFinalizada: () -> Unit
+) : AsyncTask<Void, Int, Void>() {
+
+    override fun onPreExecute() {
+        super.onPreExecute()
+        // Inicializar elementos de la UI si es necesario
+    }
+
+    override fun doInBackground(vararg params: Void?): Void? {
+        for (i in 1..100) {
+            Thread.sleep(50) // Simular retraso de red
+            publishProgress(i)
+        }
+        return null
+    }
+
+    override fun onProgressUpdate(vararg values: Int?) {
+        super.onProgressUpdate(*values)
+        values[0]?.let { actualizarProgreso(it) }
+    }
+
+    override fun onPostExecute(result: Void?) {
+        super.onPostExecute(result)
+        tareaFinalizada()
+    }
+}
